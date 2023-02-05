4a890cdf0d8822f7085d9e0db8c520c60194f467
mandysca <manu2282@gmail.com>
2023-02-04 21:06:15 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Convolutional Neural Networks (CNN)
+
+import tensorflow as tf
+
+# Keras https://keras.io API built on top of Tensor Flow 2
+
+from keras.preprocessing.image import ImageDataGenerator
+
+print(tf.__version__)
+
+# ------------------------------- Part 1 - Data pre-processing -------------------------------
+
+# processing the training set
+# apply transformation on training set to over feeding 
+
+# processing the test set
+
+
+# ------------------------------- Part 2 - Building CNN -------------------------------
+
+
+# ------------------------------- Part 3 - Training CNN -------------------------------
+
+
+# ------------------------------- Part 4 - Preditcion -------------------------------
