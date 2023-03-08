5aef614466964f4b4c6eeb3fcb5d06089345fe17
mandysca <manu2282@gmail.com>
2023-03-07 21:58:00 -0800

    Minor changes

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ train_datagen = ImageDataGenerator(
 
 # call method from the object
 train_set = train_datagen.flow_from_directory(
-    'dataset/training_set',
+    '/Users/mandeepsingh/PycharmProjects/pythonProjectCnn/dataset/training_set',
     target_size= (64, 64),
     batch_size = 32,
     class_mode = 'binary')
@@ -34,7 +34,7 @@ train_set = train_datagen.flow_from_directory(
 test_datagen = ImageDataGenerator(rescale=1/.255)
 
 test_set = test_datagen.flow_from_directory(
-    'dataset/test_set',
+    '/Users/mandeepsingh/PycharmProjects/pythonProjectCnn/dataset/test_set',
     target_size= (64, 64),
     batch_size= 32,
     class_mode= 'binary'
